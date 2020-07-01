336e3622ecf34522f81efe9e369158d42b533288
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-01 10:21:20 -0700

    count cumber of words in title test failed
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- __tests__/dino-journal.test.js
+++ __tests__/dino-journal.test.js
@@ -1,12 +1,24 @@
 import { JournalEntry } from './../src/dino-journal.js';
 
 describe('create JournalEntry object', () => {
+  let journalEntry;
+
+  beforeEach(function(){
+    journalEntry = new JournalEntry("title", "I have watched all Jurassic park movies");
+  });
 
   test('should create JournalEntry object', () => {
-    const entry = new JournalEntry("title", "entry");
-    expect(entry).toMatchObject({
+    expect(journalEntry).toMatchObject({
       title: "title",
-      entry: "entry"
+      body: "I have watched all Jurassic park movies"
     });
   });
+
+  test('should count number of words in title of JournalEntry object', () => {
+    expect(entry.numberOfWordsInTitle()).toBe(1);
+  });
+
+  /*test('should count number of words in body of JournalEntry object', () => {
+    expect(entry.numberOfWordsInEntry()).toBe(7);
+  });*/
 });

2e1b6d0943b9c4440f5afc5c6ce746c514d3fb03
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-01 10:03:03 -0700

    create JournalEntry object test failed
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ __tests__/dino-journal.test.js
@@ -0,0 +1,10 @@
+describe('create JournalEntry object', () => {
+
+  test('should create JournalEntry object', () => {
+    const entry = new JournalEntry("title", "entry");
+    expect(entry).toMatchObject({
+      title: "title",
+      entry: "entry"
+    });
+  });
+});
